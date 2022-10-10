53debf7b1c8448c5dbff9ca87c23f32f47a84193
Yonkoulong <nguyenhailong270921@gmail.com>
2022-10-11 01:57:45 +0700

    feat: build-layout-home-screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
@@ -8,26 +8,34 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text("Expense Management App"),
-      ),
-      body: Container(
-        decoration: const BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage("assets/planeAndAir.jpg"),
-                fit: BoxFit.cover)),
-        child: Center(
-          child: Container(
-            padding: EdgeInsets.all(24),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-                color: Colors.blue.shade100.withOpacity(0.6)),
-            child: const Text('Hello Word',
-                textAlign: TextAlign.center, style: TextStyle(fontSize: 22)),
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("Expense Management App"),
           ),
-        ),
-      ),
-    ));
+          body: Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/home.jpg"), fit: BoxFit.cover),
+            ),
+            padding: EdgeInsets.fromLTRB(20, 40, 20, 0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Text("Explore Beauty Of Journey",
+                    style: TextStyle(
+                        color: Colors.black54,
+                        fontSize: 44.0,
+                        fontWeight: FontWeight.bold)),
+                Text(
+                  "Everything you can imagine, is here",
+                  style: TextStyle(
+                    color: Colors.black38,
+                    fontSize: 14.0,
+                  ),
+                )
+              ],
+            ),
+          )),
+    );
   }
 }
